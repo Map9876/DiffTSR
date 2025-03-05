5ac3c2b449a344ae242c8df100a8bcf593bcc1ef
Map9876 <hhdheuwjbgg@outlook.com>
2025-03-06 04:16:43 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name='DiffTSR',
     version='0.0.1',
-    description='',
+    description='A tool for super-resolution tasks',
     packages=find_packages(),
     install_requires=[
         'torch',
         'numpy',
         'tqdm',
     ],
+    python_requires='>=3.8',  # 明确指定 Python 版本
 )
